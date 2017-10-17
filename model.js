7e7ff217b1a29be33159872c68a23bbaab2fec1c
Duaa1987 <duaa.alawad@gmail.com>
2017-10-17 16:59:05 -0500

    Add files via upload
    new file model.js

---
+++ model.js
@@ -0,0 +1,310 @@
+'use strict'
+
+const path = require('path')
+const fs = require('fs')
+const SQL = require('sql.js')
+const view = require(path.join(__dirname, 'view.js'))
+
+let _rowsFromSqlDataObject = function (object) {
+  let data = {}
+  let i = 0
+  let j = 0
+  for (let valueArray of object.values) {
+    data[i] = {}
+    j = 0
+    for (let column of object.columns) {
+      Object.assign(data[i], {[column]: valueArray[j]})
+      j++
+    }
+    i++
+  }
+  return data
+}
+
+/*
+  Return a string of placeholders for use in a prepared statement.
+*/
+let _placeHoldersString = function (length) {
+  let places = ''
+  for (let i = 1; i <= length; i++) {
+    places += '?, '
+  }
+  return /(.*),/.exec(places)[1]
+}
+
+SQL.dbOpen = function (databaseFileName) {
+  try {
+    return new SQL.Database(fs.readFileSync(databaseFileName))
+  } catch (error) {
+    console.log("Can't open database file.", error.message)
+    return null
+  }
+}
+
+SQL.dbClose = function (databaseHandle, databaseFileName) {
+  try {
+    let data = databaseHandle.export()
+    let buffer = new Buffer(data)
+    fs.writeFileSync(databaseFileName, buffer)
+    databaseHandle.close()
+    return true
+  } catch (error) {
+    console.log("Can't close database file.", error)
+    return null
+  }
+}
+
+/*
+  A function to create a new SQLite3 database from schema.sql.
+
+  This function is called from main.js during initialization and that's why
+  it's passed appPath. The rest of the model operates from renderer and uses
+  window.model.db.
+*/
+module.exports.initDb = function (appPath, callback) {
+  let dbPath = path.join(appPath, 'example.db')
+  let createDb = function (dbPath) {
+    // Create a database.
+    let db = new SQL.Database()
+    let query = fs.readFileSync(
+    path.join(__dirname, 'db', 'schema.sql'), 'utf8')
+    let result = db.exec(query)
+    if (Object.keys(result).length === 0 &&
+      typeof result.constructor === 'function' &&
+      SQL.dbClose(db, dbPath)) {
+      console.log('Created a new database.')
+    } else {
+      console.log('model.initDb.createDb failed.')
+    }
+  }
+  let db = SQL.dbOpen(dbPath)
+  if (db === null) {
+    /* The file doesn't exist so create a new database. */
+    createDb(dbPath)
+  } else {
+    /*
+      The file is a valid sqlite3 database. This simple query will demonstrate
+      whether it's in good health or not.
+    */
+    let query = 'SELECT count(*) as `count` FROM `sqlite_master`'
+    let row = db.exec(query)
+    let tableCount = parseInt(row[0].values)
+    if (tableCount === 0) {
+      console.log('The file is an empty SQLite3 database.')
+      createDb(dbPath)
+    } else {
+      console.log('The database has', tableCount, 'tables.')
+    }
+    if (typeof callback === 'function') {
+      callback()
+    }
+  }
+}
+
+/*
+  Populates the Question List.
+*/
+module.exports.getQuestions = function () {
+  let db = SQL.dbOpen(window.model.db)
+  if (db !== null) {
+    let query = 'SELECT * FROM `questions` ORDER BY `question_id` ASC'
+    try {
+      let row = db.exec(query)
+      if (row !== undefined && row.length > 0) {
+        row = _rowsFromSqlDataObject(row[0])
+				return row
+				// console.log(row)
+        // view.showQuestions(row)
+      }
+    } catch (error) {
+      console.log('model.getQuestions', error.message)
+    } finally {
+      SQL.dbClose(db, window.model.db)
+    }
+  }
+}
+
+/*
+  Fetch a question's data from the database.
+*/
+module.exports.getQuestion = function (qid) {
+  let db = SQL.dbOpen(window.model.db)
+  if (db !== null) {
+    let query = 'SELECT * FROM `questions` WHERE `question_id` IS ?'
+    let statement = db.prepare(query, [qid])
+    try {
+      if (statement.step()) {
+        let values = [statement.get()]
+        let columns = statement.getColumnNames()
+        return _rowsFromSqlDataObject({values: values, columns: columns})
+      } else {
+        console.log('model.getQuestion', 'No data found for question_id =', qid)
+      }
+    } catch (error) {
+      console.log('model.getQuestion', error.message)
+    } finally {
+      SQL.dbClose(db, window.model.db)
+    }
+  }
+}
+
+/*
+  Delete a question's data from the database.
+*/
+module.exports.deleteQuestion = function (qid, callback) {
+  let db = SQL.dbOpen(window.model.db)
+  if (db !== null) {
+    let query = 'DELETE FROM `questions` WHERE `question_id` IS ?'
+    let statement = db.prepare(query)
+    try {
+      if (statement.run([qid])) {
+        if (typeof callback === 'function') {
+          callback()
+        }
+      } else {
+        console.log('model.deleteQuestion', 'No data found for question_id =', qid)
+      }
+    } catch (error) {
+      console.log('model.deleteQuestion', error.message)
+    } finally {
+      SQL.dbClose(db, window.model.db)
+    }
+  }
+}
+
+/*
+  Insert or update form data in the database.
+*/
+module.exports.saveFormData = function (tableName, keyValue, callback) {
+	console.log('called saveFormData', keyValue);
+  if (keyValue.columns.length > 0) {
+    let db = SQL.dbOpen(window.model.db)
+    if (db !== null) {
+      let query = 'INSERT OR REPLACE INTO `' + tableName
+      query += '` (`' + keyValue.columns.join('`, `') + '`)'
+      query += ' VALUES (' + _placeHoldersString(keyValue.values.length) + ')'
+      let statement = db.prepare(query)
+      try {
+        if (statement.run(keyValue.values)) {
+          $('#' + keyValue.columns.join(', #'))
+          .addClass('form-control-success')
+          .animate({class: 'form-control-success'}, 1500, function () {
+            if (typeof callback === 'function') {
+              callback()
+            }
+          })
+        } else {
+          console.log('model.saveFormData', 'Query failed for', keyValue.values)
+        }
+      } catch (error) {
+        console.log('model.saveFormData', error.message)
+      } finally {
+        SQL.dbClose(db, window.model.db)
+      }
+    }
+  }
+}
+
+/*
+  Populates the answer List.
+*/
+module.exports.getOptions = function () {
+  let db = SQL.dbOpen(window.model.db)
+  if (db !== null) {
+    let query = 'SELECT * FROM `options` ORDER BY `option_id` ASC'
+    try {
+      let row = db.exec(query)
+      if (row !== undefined && row.length > 0) {
+        row = _rowsFromSqlDataObject(row[0])
+				return row
+				// console.log(row)
+        // view.showOptions(row)
+      }
+    } catch (error) {
+      console.log('model.getOptions', error.message)
+    } finally {
+      SQL.dbClose(db, window.model.db)
+    }
+  }
+}
+
+/*
+  Fetch an answer's data from the database.
+*/
+module.exports.getOptions = function (oid) {
+  let db = SQL.dbOpen(window.model.db)
+  if (db !== null) {
+    let query = 'SELECT * FROM `questions` WHERE `option_id` IS ?'
+    let statement = db.prepare(query, [oid])
+    try {
+      if (statement.step()) {
+        let values = [statement.get()]
+        let columns = statement.getColumnNames()
+        return _rowsFromSqlDataObject({values: values, columns: columns})
+      } else {
+        console.log('model.getOptions', 'No data found for option_id =', oid)
+      }
+    } catch (error) {
+      console.log('model.getOption', error.message)
+    } finally {
+      SQL.dbClose(db, window.model.db)
+    }
+  }
+}
+
+/*
+  Delete an answer's data from the database.
+*/
+module.exports.deleteOptions = function (oid, callback) {
+  let db = SQL.dbOpen(window.model.db)
+  if (db !== null) {
+    let query = 'DELETE FROM `optitions` WHERE `option_id` IS ?'
+    let statement = db.prepare(query)
+    try {
+      if (statement.run([oid])) {
+        if (typeof callback === 'function') {
+          callback()
+        }
+      } else {
+        console.log('model.deleteOptions', 'No data found for option_id =', oid)
+      }
+    } catch (error) {
+      console.log('model.deleteOptions', error.message)
+    } finally {
+      SQL.dbClose(db, window.model.db)
+    }
+  }
+}
+
+/*
+  Insert or update form data in the database.
+*/
+module.exports.saveFormData = function (tableName, keyValue, callback) {
+	console.log('called saveFormData', keyValue);
+  if (keyValue.columns.length > 0) {
+    let db = SQL.dbOpen(window.model.db)
+    if (db !== null) {
+      let query = 'INSERT OR REPLACE INTO `' + tableName
+      query += '` (`' + keyValue.columns.join('`, `') + '`)'
+      query += ' VALUES (' + _placeHoldersString(keyValue.values.length) + ')'
+      let statement = db.prepare(query)
+      try {
+        if (statement.run(keyValue.values)) {
+          $('#' + keyValue.columns.join(', #'))
+          .addClass('form-control-success')
+          .animate({class: 'form-control-success'}, 1500, function () {
+            if (typeof callback === 'function') {
+              callback()
+            }
+          })
+        } else {
+          console.log('model.saveFormData', 'Query failed for', keyValue.values)
+        }
+      } catch (error) {
+        console.log('model.saveFormData', error.message)
+      } finally {
+        SQL.dbClose(db, window.model.db)
+      }
+    }
+  }
+}
